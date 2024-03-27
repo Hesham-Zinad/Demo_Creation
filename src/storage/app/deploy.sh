a2643ba9ae00a3302399f4a8920d3162b3d3e78d
Hesham-Zinad <157382989+Hesham-Zinad@users.noreply.github.com>
2024-03-27 11:48:40 +0200

    Update deploy.sh

--- src/storage/app/deploy.sh
+++ src/storage/app/deploy.sh
@@ -7,8 +7,16 @@ password="Zinad#PS@2022"
 destination_directory="/home/zisoft/AMS"
 remote_script="/home/zisoft/AMS/awareness-auto-creation.sh"
 
+
+
 # Copy the file
-sshpass -p "$password" scp "$source_file" "$destination_server:$destination_directory"
+pwd
+cd /var/www/storage/app
+pwd
+sshpass -p "$password" scp -o StrictHostKeyChecking=no "$source_file" "$destination_server:$destination_directory"
+echo "copy input txt file succssfuly "
 
 # Execute file
-# sshpass -p "$password" ssh "$destination_server" "bash $remote_script"
+sshpass -p "$password" ssh  -o StrictHostKeyChecking=no "$destination_server"  "cd /home/zisoft/AMS/ && echo 'Zinad#PS@2022' | sudo -S ./awareness-auto-creation.sh" 
+#echo "execute auto-demo bash scribt"
+
